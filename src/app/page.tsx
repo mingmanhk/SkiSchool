b2ff1ae3563c4f9fc3b81a979380a321edf9a51d
mingmanhk <mingmanhk@gmail.com>
2026-01-20 07:15:33 +0000

    Debug: Disable root redirect to test deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 
-import { redirect } from 'next/navigation';
-
 export default function RootPage() {
-  redirect('/en');
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1>Ski School OS is Running! (Root Page)</h1>
+    </div>
+  );
 }
